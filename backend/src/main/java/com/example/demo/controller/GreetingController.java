40598118302621c2754d037d3b263e2089688479
nishthatripathi-wtg <nishtha.tripathi@wisetechhlobal.com>
2026-03-16 16:49:10 +0530

    Test db

--- backend/src/main/java/com/example/demo/controller/GreetingController.java
+++ backend/src/main/java/com/example/demo/controller/GreetingController.java
@@ -1,12 +1,18 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
 import java.util.Map;
 
+@EnableAutoConfiguration
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class GreetingController {
